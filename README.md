2543be284c84fc0a5cb628b43f46aa99f8072ccc
Scott Green <sagreenxyz@gmail.com>
2022-01-23 12:01:57 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Mapping with Leaflet
+
+This is an activity for Lesson 5.7.5- Leaflet.
+
+Please refer to the Activity Guide in Canvas for directions.
---
+++ public/app.js
@@ -0,0 +1,19 @@
+// create map
+
+
+// add openstreetmap tiles
+
+
+// create and main add geolocation marker
+
+
+// draw the 2nd arrondissement
+
+
+// create red pin marker
+
+
+// metro station markers
+
+
+
